081b831db9460ae9d6da138efa619c801c9297f7
Vlad Baidyk <102901907+Vladislav-Baidyk@users.noreply.github.com>
2026-03-23 13:28:53 +0200

    Delete javaScript directory

--- javaScript/hm_02/hm_02_02/main.js
+++
@@ -1,7 +0,0 @@
-/*– Створити 3 об’єкти які описують книги. Поля об’єкту : title ,pageCount, genre. */
-let book1 = {title: 'Dartanian',pageCount:23,genre : 'medieval' };
-let book2 = {title: 'Dune',pageCount:12,genre: 'fantastika'};
-let book3 = {title: 'Star Wars', pageCount:43,genre: 'fantastika'};
-console.log(book1);
-console.log(book2);
-console.log(book3);
--- javaScript/hm_02/hm_02_03/main.js
+++
@@ -1,5 +0,0 @@
-/*– Створити 3 об’єкти, які описують книги. Поля об’єкту : title ,pageCount, genre, authors. 
-Поле “автори” – являється  масивом. Кожен автор має поля name та age. */
-let book1 = {title: 'Dartanian',pageCount:23,genre : 'medieval',author : ['Дюма' , 1802] };
-let book2 = {title: 'Attack of the Titan',pageCount:123,genre : 'fantasy',author : ['Саяма' , 1982] };
-let book3 = {title: 'Пепіл й кісті',pageCount:233,genre : 'fantasy',author : ['Гріша Верс' , 2007] };
